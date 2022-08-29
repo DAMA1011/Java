e52fc189ceff6b2c112aa383ccefbd3a24292630
DAMA1011 <oldguy911011@gmail.com>
2022-08-29 16:35:35 +0800

    2022/08/29 school

---
+++ JavaLab/src/extend/TestProduct.java
@@ -0,0 +1,18 @@
+package extend;
+
+public class TestProduct {
+
+	public static void main(String[] args) {
+		
+		Product pd = new Product("筆記本", 50);
+		System.out.printf("名稱: %s, 價錢: %d%n", pd.getName(), pd.getPrice());
+
+		Notebook nb = new Notebook("Asus", 30000, 365);
+		System.out.printf("名稱: %s, 價錢: %d%n", nb.getName(), nb.getPrice());
+		System.out.println(nb.name); // 同一個 package 才能有 protected 效果
+		System.out.println(nb.disc());
+		
+		
+	}
+
+}
--- JavaLab/src/func/People.java
+++ JavaLab/src/func/People.java
@@ -3,7 +3,8 @@ package func;
 public class People {
 
 	// static: 表示該屬性屬於類別，稱為類別成員/靜態屬性
-	static double overWeightBMI = 24; // 設定一個常數，一樣是一個屬性
+	// final: 不可改變後方內容
+	static final double overWeightBMI = 24; // 設定一個常數，一樣是一個屬性
 	// 靜態方法不能用到物件屬性，因為不需要物件(new)就可以呼叫
 	public static double BMI(double h, double w) {
 		System.out.println(People.overWeightBMI);
