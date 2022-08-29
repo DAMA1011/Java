60dbd2ce32f27528d57a5f28cc7df235e6749d0e
DAMA1011 <oldguy911011@gmail.com>
2022-08-29 10:05:57 +0800

    2022/08/29 school

--- JavaLab/src/func/People.java
+++ JavaLab/src/func/People.java
@@ -2,14 +2,20 @@ package func;
 // 類別(設計圖)
 public class People {
 
+	// static: 表示該屬性屬於類別，稱為類別成員/靜態屬性
+	static double overWeightBMI = 24; // 設定一個常數，一樣是一個屬性
+	// 靜態方法不能用到物件屬性，因為不需要物件(new)就可以呼叫
+	public static double BMI(double h, double w) {
+		System.out.println(People.overWeightBMI);
+		return w / (h *h);
+	}
+	
 	double height; // 屬性
 	double weight;
-	//static: 表示該屬性屬於類別，稱為類別成員/靜態屬性
-	static double overWeightBMI = 24; // 設定一個常數，一樣是一個屬性
-	
-	// 物件方法:
+	// 物件屬性跟方法: 
 	public double getBMI() {
-		double bmi = weight/(height*height);
+//		double bmi = weight/(height*height);
+		double bmi = People.BMI(height, weight);
 		return bmi;
 		
 	}
