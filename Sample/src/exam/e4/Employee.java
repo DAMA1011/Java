a759b58031dd75cc5b786769676d653d26a09576
DAMA1011 <oldguy911011@gmail.com>
2022-08-29 18:44:51 +0800

    2022/08/28 home

--- Sample/src/exam/e4/Employee.java
+++
@@ -1,7 +0,0 @@
-package exam.e4;
-
-public class Employee {
-    private int empno;
-    private String name;
-    private long salary;
-}
--- Sample/src/inner/TestInnerClass.java
+++
@@ -1,8 +0,0 @@
-package inner;
-
-public class TestInnerClass {
-    public static void main(String[] args) {
-        OuterClass a = new OuterClass(1.7,60);
-        a.printBMI();         
-    }
-}
