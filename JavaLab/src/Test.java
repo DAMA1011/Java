45284cbb8d4fe4dce7f2cf7f13f8fc47a0be72d8
DAMA1011 <oldguy911011@gmail.com>
2022-09-03 16:32:34 +0800

    2022/09/03 school

--- JavaLab/src/Test.java
+++ JavaLab/src/Test.java
@@ -1,12 +1,16 @@
+//3.程式如下，請問compile會有錯嗎，如果有錯請問錯誤原因為何?
+
 public class Test {
-private int i = 1;
-public static void main(String argv[]){
-int i = 2;
-Test s = new Test();
-s.print();
-}
+	private int i = 1;
+
+	public static void main(String argv[]) {
+		int i = 2;
+		Test s = new Test();
+		s.print();
+	}
+
 //DO NOT CHANGE
-public static void print(){
-System.out.println(i);
-}
+	public static void print() {
+		System.out.println(i);
+	}
 }
