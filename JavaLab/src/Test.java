2bfc2bc7af00fcdf8cab3395327c6510d2b63628
DAMA1011 <oldguy911011@gmail.com>
2022-09-03 15:49:06 +0800

    2022/09/03 school

---
+++ JavaLab/src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+private int i = 1;
+public static void main(String argv[]){
+int i = 2;
+Test s = new Test();
+s.print();
+}
+//DO NOT CHANGE
+public static void print(){
+System.out.println(i);
+}
+}
