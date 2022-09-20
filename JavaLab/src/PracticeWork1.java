4454eabb10769e849bd21e988adfdab165d02c66
DAMA1011 <oldguy911011@gmail.com>
2022-09-20 23:15:36 +0800

    2022/09/20 home

---
+++ JavaLab/src/PracticeWork1.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class PracticeWork1 {
+
+	public static void main(String[] args) {
+
+//		{ // ex.1 選擇性敘述的練習-season
+//			
+//			Scanner input = new Scanner(System.in);
+//			System.out.println("請輸入月份:");
+//			int month = input.nextInt();
+//			
+//			switch (month) {
+//			case 3: 
+//			case 4:
+//			case 5:
+//				System.out.printf("%d 月為春%n", month); // 3,4,5
+//				break;
+//			case 6: 
+//			case 7:
+//			case 8:
+//				System.out.printf("%d 月為夏%n", month); // 6,7,8
+//				break;
+//			case 9: 
+//			case 10:
+//			case 11:
+//				System.out.printf("%d 月為秋%n", month); // 9,10,11
+//				break;
+//			case 12: 
+//			case 1:
+//			case 2:
+//				System.out.printf("%d 月為冬%n", month); // 12,1,2
+//				break;
+//			default :
+//				System.out.printf("輸入錯誤%n");
+//			
+//			input.close();
+//				
+//			}
+//			
+//		}
+		
+//		{ // ex.2 選擇性敘述的練習-salary
+//			
+//			Scanner input = new Scanner(System.in);
+//			System.out.println("請輸入時數:");
+//			double time = input.nextInt();
+//			
+//			if (time <= 60) {
+//				double salary = time * 120;
+//				System.out.printf("薪資為 %.0f%n", salary);
+//			} else if (time > 61 && time < 80) {
+//				double salary = 60 * 120 + (time - 60) * 120 * 1.25;
+//				System.out.printf("薪資為 %.0f%n", salary);
+//			} else {
+//				double salary = 60 * 120 + 20 * 120 * 1.25 + (time - 80) * 120 * 1.5;
+//				System.out.printf("薪資為 %.0f%n", salary);
+//			}
+//			
+//			input.close();
+//			
+//		}
+		
+		{ // ex.3 選擇性敘述的練習-leapYear
+			
+			Scanner input = new Scanner(System.in);
+			System.out.println("請輸入年份:");
+			int year = input.nextInt();
+			
+			if () {
+				
+			} else if () {
+				
+			} else {
+				System.out.printf("%d 年為平年%n");
+			}
+			
+			input.close();
+			
+		}
+		
+		{ // ex.4 選擇性敘述的練習-refund
+			
+			
+			
+		}
+		
+	}
+
+}
